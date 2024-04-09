a1a5872b15d918718be18566fd1126fcb47162a2
Scott Feichter <scottfeichter@gmail.com>
2024-04-09 13:23:15 -0700

    added bonus from class walkthrough

--- server.js
+++ server.js
@@ -48,6 +48,38 @@ const server = http.createServer((req, res) => {
       return res.end();
     }
 
+    if(req.method === `GET` && req.url === `/dogs/`) {
+      console.log("req ulr: ", res.url);
+      const urlParts = req.url.split("/");
+      console.log("url part: ", urlParts);
+      if(urlParts.length === 3) {
+        const dogId = urlParts[2];
+        if(dogId === "new") {
+          const responseBody = "Dog create form page";
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          return res.end(responseBody);
+        } else {
+          const responseBody = `Dog details for dogId: ${dogId}`;
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          return res.end(responseBody);
+        }
+    }
+
+
+    if(req.method === "POST" && req.url.startsWith("/dogs/")) {
+      console.log("req ulr: ", res.url);
+      const urlParts = req.url.split("/");
+      console.log("url part: ", urlParts);
+      if(urlParts.length === 3) {
+        const dogId = urlParts[2];
+        res.statusCode = 302;
+        res.setHeader(`location`, `/dogs/${dogId}`);
+        return res.end();
+      }
+    }
+
     // Do not edit below this line
     // Return a 404 response when there is no matching route handler
     res.statusCode = 404;
